b7dfaf170d00e00a3e55984e9eeaaebfdf530049
Andrew <fastroute66@live.com>
2020-04-14 12:34:31 -0500

    Add project files.

---
+++ ToDoList/ToDoForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ToDoList
+{
+    partial class TodoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clsToDo = new System.Windows.Forms.CheckedListBox();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.txtToDo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstDone = new System.Windows.Forms.ListBox();
+            this.chkUrgent = new System.Windows.Forms.CheckBox();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clsToDo
+            // 
+            this.clsToDo.FormattingEnabled = true;
+            this.clsToDo.Location = new System.Drawing.Point(22, 109);
+            this.clsToDo.Name = "clsToDo";
+            this.clsToDo.Size = new System.Drawing.Size(600, 259);
+            this.clsToDo.TabIndex = 0;
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Location = new System.Drawing.Point(533, 60);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(89, 23);
+            this.btnAddTask.TabIndex = 1;
+            this.btnAddTask.Text = "Add";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // txtToDo
+            // 
+            this.txtToDo.Location = new System.Drawing.Point(22, 46);
+            this.txtToDo.Name = "txtToDo";
+            this.txtToDo.Size = new System.Drawing.Size(395, 22);
+            this.txtToDo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "What do you need to do?";
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Location = new System.Drawing.Point(22, 390);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(211, 23);
+            this.btnRemoveItem.TabIndex = 4;
+            this.btnRemoveItem.Text = "Remove Checked Items";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 439);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Deleted Items";
+            // 
+            // lstDone
+            // 
+            this.lstDone.FormattingEnabled = true;
+            this.lstDone.ItemHeight = 16;
+            this.lstDone.Location = new System.Drawing.Point(22, 461);
+            this.lstDone.Name = "lstDone";
+            this.lstDone.Size = new System.Drawing.Size(600, 84);
+            this.lstDone.TabIndex = 7;
+            // 
+            // chkUrgent
+            // 
+            this.chkUrgent.AutoSize = true;
+            this.chkUrgent.Location = new System.Drawing.Point(423, 62);
+            this.chkUrgent.Name = "chkUrgent";
+            this.chkUrgent.Size = new System.Drawing.Size(81, 21);
+            this.chkUrgent.TabIndex = 8;
+            this.chkUrgent.Text = "Urgent?";
+            this.chkUrgent.UseVisualStyleBackColor = true;
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(92, 74);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(324, 24);
+            this.cbxCategory.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Category";
+            // 
+            // TodoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 557);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxCategory);
+            this.Controls.Add(this.chkUrgent);
+            this.Controls.Add(this.lstDone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtToDo);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.clsToDo);
+            this.Name = "TodoForm";
+            this.Text = "ToDo List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clsToDo;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.TextBox txtToDo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRemoveItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstDone;
+        private System.Windows.Forms.CheckBox chkUrgent;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ToDoList/ToDoForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**********************************\
+*                                  *
+* Andrew Terwilliger 4/2/2020      *
+* Minneapolis College              *
+* ITEC 2505-60 C# Programming      *
+*                                  *
+\**********************************/
+
+namespace ToDoList
+{
+    public partial class TodoForm : Form
+    {
+        public TodoForm()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbxCategory.Items.Add("Work");
+            cbxCategory.Items.Add("School");
+            cbxCategory.Items.Add("Personal");
+            cbxCategory.SelectedIndex = 0;
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            string newItem = txtToDo.Text.Trim();
+            string category = cbxCategory.SelectedItem.ToString();
+            bool urgent = chkUrgent.Checked;
+
+            Task newTask = new Task(newItem, category, urgent);
+
+            if (!string.IsNullOrEmpty(newTask.details))
+            {
+                string addingTask = newTask.GetTask();
+
+                if (itemsIsInList(clsToDo.Items, addingTask))
+                {
+                    MessageBox.Show("You already added that item.", "Error");
+                }
+                else
+                {
+                    clsToDo.Items.Add(addingTask);
+                    txtToDo.Text = "";
+                }
+            }
+        }
+
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            List<String> doneItems = new List<string>();
+
+
+            if (clsToDo.CheckedItems.Count.Equals(0))
+            {
+                MessageBox.Show("Nothing is selected to remove.", "Error");
+            } else
+            {
+                foreach (string Task in clsToDo.CheckedItems)
+                {
+                    doneItems.Add(Task);
+                }
+                foreach (string item in doneItems)
+                {
+                    clsToDo.Items.Remove(item);
+                    lstDone.Items.Add(item);
+                }
+            }
+        }
+        private bool itemsIsInList(CheckedListBox.ObjectCollection items, string newItem)
+        {
+            foreach (string item in items)
+            {
+                if (item.ToUpper() == newItem.ToUpper())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public class Task
+    {
+        public string details = string.Empty;
+        public string category = string.Empty;
+        public bool urgent = false;
+        public DateTime created = DateTime.Now;
+
+        public Task(string Details, string Category, bool Urgent)
+        {
+            details = Details;
+            category = Category;
+            urgent = Urgent;
+        }
+
+        public string GetTask()
+        {
+            string task = $"{category} | {details} - Created At {created}";
+            if (urgent) task += " URGENT!";
+
+            return task;
+        }
+
+    }
+}
