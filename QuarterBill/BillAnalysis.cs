dcfd8a4846d37c2212e180a426efe6a18cf44863
Andrew <fastroute66@live.com>
2020-04-28 12:13:31 -0500

    Updating code with Comments

--- QuarterBill/BillAnalysis.cs
+++ QuarterBill/BillAnalysis.cs
@@ -4,26 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**********************************\
+*                                  *
+* Andrew Terwilliger 4/28/2020     *
+* Minneapolis College              *
+* ITEC 2505-60 C# Programming      *
+*                                  *
+\**********************************/
+
 namespace QuarterBill
 {
     class BillAnaylisis
     {
+        // Public methods set readonly for getting the Average and Total values.
         public string Average { get; private set; }
         public string Total { get; private set; }
 
+        // Public method to calculate a bill, taking in the List of quarter and calculating their total and average.
+        // Rewritten in a way to accept more than 4 quarters if needed, calculating the average based on how many values are in the list.
         public void calculateBill(List<Decimal> Quarters)
         {
             // Preparing variables;
             decimal total = 0m;
             decimal average = 0m;
 
-            // Once all the inputs have been validated and stored - we go through, add them together and figure out the average.
+            // Looping through and adding their total and average for calculation.
             foreach (decimal quarter in Quarters)
             {
                 total += quarter;
                 average += quarter;
             }
-            Average = (average / 4).ToString("C");
+
+            // Setting the internal values of Average and Total.
+            Average = (average / Quarters.Count).ToString("C");
             Total = total.ToString("C");
         }
         
--- QuarterBill/Form1.cs
+++ QuarterBill/Form1.cs
@@ -9,17 +9,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**********************************\
+*                                  *
+* Andrew Terwilliger 4/28/2020     *
+* Minneapolis College              *
+* ITEC 2505-60 C# Programming      *
+*                                  *
+\**********************************/
+
 namespace QuarterBill
 {
     public partial class Form1 : Form
     {
-        // Global list of Quarters.
-        readonly List<Decimal> Quarters = new List<Decimal>();
+        // Global list of Quarters and their TextBoxes.
+        readonly List<Decimal> QuarterBills = new List<Decimal>();
         readonly List<TextBox> QtrBoxes = new List<TextBox>();
 
         public Form1()
         {
             InitializeComponent();
+            
+            // Adding all quarters to a list for easy looping.
             QtrBoxes.Add(txbQtrOne);
             QtrBoxes.Add(txbQtrTwo);
             QtrBoxes.Add(txbQtrThree);
@@ -30,37 +40,42 @@ namespace QuarterBill
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             // Clearing the Quarters list, and setting the error status as false.
-            Quarters.Clear();
+            QuarterBills.Clear();
             bool errored = false;
 
             // Bill class to calculate our bill.
             BillAnaylisis Bill = new BillAnaylisis();
 
-            // Validating all the inputs provided by the user before going ahead and adding to list.
-
-            while (errored != true && Quarters.Count < 4)
+            // Looping while the loop has not errored, and while the count is less than 4.
+            while (errored != true && QuarterBills.Count < 4)
             {
+                // Looping for every TextBox in the List of TextBoxes.
                 foreach (TextBox quarter in QtrBoxes)
                 {
+                    // If it has errored due to a previous loop, we continue the loop out.
                     if (errored == true) continue;
+
+                    // Validating if the TextBox has the correct data we need.
                     if (!ValidatePositiveDecimal(quarter.Text, out decimal Value, out string QtrError))
                     {
+                        // If not, we focus - error and notify the User.
                         quarter.Focus();
                         errored = true;
                         MessageBox.Show($"Please verify the text in the highlighted quarter. {QtrError}", "Quarter Error");
                     }
                     else
                     {
-                        Quarters.Add(Value);
+                        // Elsewise we add the value to the QuarterBill list.
+                        QuarterBills.Add(Value);
                     }
                 }
             }
 
             // Validating if the count has all the quarters before continuing.
-            if (Quarters.Count == 4)
+            if (QuarterBills.Count == 4)
             {
                 // Have the Class calculate the bill.
-                Bill.calculateBill(Quarters);
+                Bill.calculateBill(QuarterBills);
 
                 // Post the total and average in currency.
                 txbTotal.Text = Bill.Total;
